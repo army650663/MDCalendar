79e84426a4d3ffe58e5ffb3f8bf14147d3dbe9e8
MadmanChen <madmanchen@hsinten.com.tw>
2017-04-21 16:08:47 +0800

    Fix

--- sample/src/main/java/tw/idv/madmanchen/mdcalendar/MainActivity.java
+++ sample/src/main/java/tw/idv/madmanchen/mdcalendar/MainActivity.java
@@ -3,7 +3,7 @@ package tw.idv.madmanchen.mdcalendar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-//Test
+//Test ee
 public class MainActivity extends AppCompatActivity {
 
     @Override
